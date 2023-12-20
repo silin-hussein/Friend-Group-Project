5a525c1b7a873efdd969a44ea35e96c91f3e3c4a
JakobMichel <jakob.michel25@gmail.com>
2023-12-20 14:57:57 +0100

    added sample js code

--- backend/jsonFileManager.js
+++ backend/jsonFileManager.js
@@ -1,1 +1,30 @@
-console.log("hi");
+post();
+
+function get() {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+    }
+    };
+
+    req.open("GET", "https://api.jsonbin.io/v3/b/6582e462266cfc3fde6bcba5/latest", true);
+    req.setRequestHeader("X-Master-Key", "$2a$10$Aat4wCb8nzm8up2GEGaRle6OwswNkxbnbYOuvY9oX6Kmqx7JNl.hS");
+    req.send();
+}
+
+function post() {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+    }
+    };
+
+    req.open("PUT", "https://api.jsonbin.io/v3/b/6582e462266cfc3fde6bcba5", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("X-Master-Key", "$2a$10$Aat4wCb8nzm8up2GEGaRle6OwswNkxbnbYOuvY9oX6Kmqx7JNl.hS");
+    req.send('{"sample": "Hello World"}');
+}
