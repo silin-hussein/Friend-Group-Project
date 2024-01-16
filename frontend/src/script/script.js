9e64b446fad55b4e0bc32aaedbf4061d1f325e3f
Susu26 <s.hussein@students.htl-leonding.ac.at>
2024-01-16 21:54:02 +0100

    trying to fix big errors

--- frontend/src/script/script.js
+++ frontend/src/script/script.js
@@ -11,7 +11,8 @@ function sendName() {
     username = document.getElementById("enterName").value;
     if (checkUsername(username)) {
         document.getElementById("namePopup").style.display = "none";
-        document.getElementById("logo-start").style.display = "none";
+        document.getElementById("login").style.display = "none";
+       
         document.body.style.backgroundColor = "white";
 
         initMessages();
@@ -24,16 +25,6 @@ function sendName() {
             <button onclick="printChatLog()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/>
             </svg></button>
-
-
-            <div style="width: 100%; height: 100%; padding-top: 17px; padding-bottom: 17px; padding-left: 50px; padding-right: 75px; background: #127C87; border-top-left-radius: 25px; border-top-right-radius: 25px; overflow: hidden; justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
-            <div style="height: 60px; padding-top: 23px; padding-bottom: 14px; padding-left: 28.83px; padding-right: 842.45px; background: white; border-radius: 16px; overflow: hidden; justify-content: flex-start; align-items: center; display: flex">
-                <div style="width: 231.71px; height: 23px; opacity: 0.50; background: #127C87"></div>
-            </div>
-            <div style="width: 61px; height: 60px; position: relative; background: white; border-radius: 16px; overflow: hidden">
-                <div style="width: 20px; height: 24.58px; left: 20px; top: 18px; position: absolute; background: #60D4AB"></div>
-            </div>
-    </div>
     `;
     }
 }
