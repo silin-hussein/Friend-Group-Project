708f7c44d3f41dc6084ef7c5a0d88237f48eb59f
KnechtenSami <s.mayer@students.htl-leonding.ac.at>
2024-02-02 18:01:51 +0100

    setting up tailwind css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import()} */
+/** @type {import('tailwindcss').default} */
 module.exports = {
   content: ['./public/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        original: {
+          primary: '#127C87',
+          secondary: '#65B8BE',
+          text: '#0F0F0F',
+          background: '#F7FCFA',
+          accent: '#D1F3F3'
+        },
+        // Here you can add as many custom color palletes as you want
+      },
+    },
   },
   plugins: [],
 }
