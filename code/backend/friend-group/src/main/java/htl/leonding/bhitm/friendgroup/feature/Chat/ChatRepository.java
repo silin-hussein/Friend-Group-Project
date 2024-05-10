3291a95c0224591a56f61ace0554a55b71279b3d
KnechtenSami <s.mayer@students.htl-leonding.ac.at>
2024-05-10 12:35:05 +0200

    changing up java project structure

--- code/backend/friend-group/src/main/java/htl/leonding/bhitm/friendgroup/feature/Chat/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package htl.leonding.bhitm.friendgroup.feature.Chat;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class ChatRepository implements PanacheRepository<Chat> {
-
-}
--- code/backend/friend-group/src/main/java/htl/leonding/bhitm/friendgroup/feature/Chat/Message/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package htl.leonding.bhitm.friendgroup.feature.Chat.Message;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MessageRepository implements PanacheRepository<Message> {
-
-}
